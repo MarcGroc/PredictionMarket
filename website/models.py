ac6c90d83a34e4d0fabc6707f0f3d99358fecb71
MarcGroc <marcgroc12@gmail.com>
2022-08-23 10:08:34 +0200

    Event model created

---
+++ website/models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Event(models.Model):
+    YES_NO_CHOICE = (
+        ('Yes', 'Yes'),
+        ('No', 'No')
+    )
+    title = models.CharField(max_length=100, unique=True, null=False)
+    category = models.CharField(max_length=20, null=False)
+    expiration_date = models.DateField(null=False)
+    volume = models.DecimalField(max_digits=20, decimal_places=2, default=1)
+    date_created = models.DateTimeField(default=timezone.now)
+    choice = models.CharField(max_length=3, choices=YES_NO_CHOICE, default='-')
+
+    def __str__(self):
+        return self.title
+
+    def not_older_than_five_days(self):
+        return (datetime.date.today() - self.date_created).days < 5
+
+
